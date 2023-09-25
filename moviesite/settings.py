6c3494d8035b8a00ee332b1df68a1255b00710dc
gui.boen <gui.boen@usp.br>
2023-09-25 17:54:31 -0300

    Inserir Listagem, busca e criação de filmes

--- moviesite/settings.py
+++ moviesite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'staticpages.apps.StaticpagesConfig', # adicione esta linha
-
+    'movies.apps.MoviesConfig', # adicione esta linha
 ]
 
 MIDDLEWARE = [
